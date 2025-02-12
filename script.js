8dcbb37d06db2146f17e6b29ec4e22aef0779ed2
Michael Siemens Uy <mpuy3@up.edu.ph>
2025-02-13 02:37:22 +0800

    added gestures

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
-const modelViewer = document.getElementById('model-viewer');
+sceneEl.addEventListener("onefingermove", handleRotation);
+sceneEl.addEventListener("twofingermove", handleScale);
 
-// Optional: Handle events or customize behavior here
-modelViewer.addEventListener('load', () => {
-    console.log('Model loaded!');
+
+sceneEl.addEventListener("markerFound", (e) => {
+    isMarkerVisible = true;
 });
 
-modelViewer.addEventListener('ar-status', (event) => {
-    console.log(`AR status: ${event.detail.status}`);
+sceneEl.addEventListener("markerLost", (e) => {
+    isMarkerVisible = false;
 });
+
+function handleRotation(event) {
+    if (isMarkerVisible) {
+      el.object3D.rotation.y +=
+        event.detail.positionChange.x * rotationFactor;
+
+      el.object3D.rotation.x +=
+        event.detail.positionChange.y * rotationFactor;
+    }
+  };
+
+function handleScale(event) {
+    if (isMarkerVisible) {
+      this.scaleFactor *=
+        1 + event.detail.spreadChange / event.detail.startSpread;
+
+      this.scaleFactor = Math.min(
+        Math.max(this.scaleFactor, this.data.minScale),
+        this.data.maxScale
+      );
+
+      el.object3D.scale.x = scaleFactor * initialScale.x;
+      el.object3D.scale.y = scaleFactor * initialScale.y;
+      el.object3D.scale.z = scaleFactor * initialScale.z;
+    }
+  };
+
+  
 
